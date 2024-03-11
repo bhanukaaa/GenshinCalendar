bd758a6181a11e6929e7a0f5505fd3912f691a99
Bhanuka <wolfaiih@gmail.com>
2024-03-11 12:27:10 +0530

    Update

---
+++ python_script/Characters.js
@@ -0,0 +1,9 @@
+const Characters = {
+0 : {name: "albedo", displayName: "Albedo", element: "geo", selected: false, material: "ballad"},
+1 : {name: "alhaitham", displayName: "Alhaitham", element: "dendro", selected: false, material: "ingenuity"},
+2 : {name: "aloy", displayName: "Aloy", element: "cryo", selected: false, material: "freedom"},
+3 : {name: "amber", displayName: "Amber", element: "pyro", selected: false, material: "freedom"},
+4 : {name: "itto", displayName: "Arataki Itto", element: "geo", selected: false, material: "elegance"},
+} 
+
+export default Characters
