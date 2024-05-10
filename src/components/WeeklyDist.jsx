f43bfca0c1126a7d9ea4defa2e737ac7e42fb3a3
Bhanuka <wolfaiih@gmail.com>
2024-05-10 12:38:17 +0530

    update weeklydist

--- src/components/WeeklyDist.jsx
+++ src/components/WeeklyDist.jsx
@@ -3,71 +3,44 @@ import classes from "./WeeklyDist.module.css";
 import Portrait from "./Portrait";
 
 function WeeklyDist(data) {
+  let days = [
+    "None",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let weekElements = [];
+
+  for (let i = 1; i < 7; i++) {
+    weekElements.push(
+      <div className={classes.week}>
+        <h2 className={classes.heading}>{days[i]}</h2>
+        <div className={classes.portraitContainer}>
+          {data["data"][i].length > 0 ? (
+            data["data"][i].map((char) => <Portrait name={char} key={char} />)
+          ) : (
+            <h3 className={classes.heading}>
+              No Selected Characters are Farmable on This Day
+            </h3>
+          )}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className={classes.group}>
-      <div className={classes.week}>
-        <h2 className={classes.heading}>Monday</h2>
-        <div className={classes.portraitContainer}>
-          {data["data"][1].length > 0 ? (
-            data["data"][1].map((char) => <Portrait name={char} key={char} />)
-          ) : (
-            <h3 className={classes.heading}>No Selected Characters are Farmable on This Day</h3>
-          )}
-        </div>
-      </div>
-      <div className={classes.week}>
-        <h2 className={classes.heading}>Tuesday</h2>
-        <div className={classes.portraitContainer}>
-          {data["data"][2].length > 0 ? (
-            data["data"][2].map((char) => <Portrait name={char} key={char} />)
-          ) : (
-            <h3 className={classes.heading}>No Selected Characters are Farmable on This Day</h3>
-          )}
-        </div>
-      </div>
-      <div className={classes.week}>
-        <h2 className={classes.heading}>Wednesday</h2>
-        <div className={classes.portraitContainer}>
-          {data["data"][3].length > 0 ? (
-            data["data"][3].map((char) => <Portrait name={char} key={char} />)
-          ) : (
-            <h3 className={classes.heading}>No Selected Characters are Farmable on This Day</h3>
-          )}
-        </div>
-      </div>
-      <div className={classes.week}>
-        <h2 className={classes.heading}>Thursday</h2>
-        <div className={classes.portraitContainer}>
-          {data["data"][4].length > 0 ? (
-            data["data"][4].map((char) => <Portrait name={char} key={char} />)
-          ) : (
-            <h3 className={classes.heading}>No Selected Characters are Farmable on This Day</h3>
-          )}
-        </div>
-      </div>
-      <div className={classes.week}>
-        <h2 className={classes.heading}>Friday</h2>
-        <div className={classes.portraitContainer}>
-          {data["data"][5].length > 0 ? (
-            data["data"][5].map((char) => <Portrait name={char} key={char} />)
-          ) : (
-            <h3 className={classes.heading}>No Selected Characters are Farmable on This Day</h3>
-          )}
-        </div>
-      </div>
-      <div className={classes.week}>
-        <h2 className={classes.heading}>Saturday</h2>
-        <div className={classes.portraitContainer}>
-          {data["data"][6].length > 0 ? (
-            data["data"][6].map((char) => <Portrait name={char} key={char} />)
-          ) : (
-            <h3 className={classes.heading}>No Selected Characters are Farmable on This Day</h3>
-          )}
-        </div>
-      </div>
+      {weekElements}
+
       <div className={classes.week}>
         <h2 className={classes.heading}>Sunday</h2>
-        <h3 className={classes.heading}>All Characters are Farmable on This Day</h3>
+        <h3 className={classes.heading}>
+          All Characters are Farmable on This Day
+        </h3>
       </div>
     </div>
   );
